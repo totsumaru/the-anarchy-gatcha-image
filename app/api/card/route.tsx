1d8ea450a5b2ff5af079cb0723463551d02b005e
totsuka <techstart35@gmail.com>
2023-10-18 22:25:18 +0900

    fix

--- app/api/card/route.tsx
+++ app/api/card/route.tsx
@@ -2,11 +2,54 @@ import { ImageResponse } from 'next/server';
 
 export const runtime = 'edge';
 
+const bronzeImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1164187695409528995/bronze.jpg?ex=65424d48&is=652fd848&hm=3d680ab25319aeefa3463045a2b20da571ae77a2504212e06f2cd9515c882e32&"
+const silverImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1164187697213096058/silver.jpg?ex=65424d48&is=652fd848&hm=23efc61d9c501cff11561da82174398ecefbda6dd443cfb7399fed45aca6155e&"
+const goldImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1164187696512630844/gold.jpg?ex=65424d48&is=652fd848&hm=73209f98fbf026ee3b14feae83b2b9b8ea501306091bbaa2011d43942c87eaf7&"
+const platinumImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1164187696915296326/platinum.jpg?ex=65424d48&is=652fd848&hm=3876e64eef32f97027801c7f9a240952924b5e8c0e9b874afd809e77d9a63783&"
+const diamondImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1164187696206463017/diamond.jpg?ex=65424d48&is=652fd848&hm=e55b343ff50a84e9e1ba54fde0e6fb8b32b072f4f5eae2b0125e28ced865f2ac&"
+const crazyImageURL = "https://cdn.discordapp.com/attachments/1103240223376293938/1164187695766056980/crazy.jpg?ex=65424d48&is=652fd848&hm=d44df3844f4808328ca957baff5bd36d81fe0e0172cc8a6612edfcf5046a7c27&"
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const username = searchParams.get('username');
   const avatar = searchParams.get('avatar');
   const point = searchParams.get('point');
+
+  let imageURL = ""
+  switch (point) {
+    case "6":
+    case "7":
+    case "8":
+      imageURL = bronzeImageURL
+      break
+    case "9" :
+    case "10" :
+    case "11":
+      imageURL = silverImageURL
+      break
+    case "12" :
+    case "13" :
+    case "14":
+      imageURL = goldImageURL
+      break
+    case "15":
+    case "16":
+    case "17":
+      imageURL = platinumImageURL
+      break
+    case "18":
+    case "19" :
+    case "20":
+      imageURL = diamondImageURL
+      break
+    case "21" :
+    case "22" :
+    case "23":
+      imageURL = crazyImageURL
+      break
+    default:
+      return new ImageResponse(<></>)
+  }
 
   return new ImageResponse(
     (
@@ -15,7 +58,7 @@ export async function GET(request: Request) {
           display: 'flex',
           fontSize: 60,
           color: 'black',
-          backgroundImage: `url(https://cdn.discordapp.com/attachments/984614055681613867/1164171311271120997/bronze-min.jpg?ex=65423e05&is=652fc905&hm=0f73efe62e28255a0c219ea96f8f6359fe5c1614efb6c0ef7d6dba7b5a311c0e&)`,
+          backgroundImage: `url(${imageURL})`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
           backgroundSize: "100% 100%",
